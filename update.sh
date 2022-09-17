5459fafe6e710e6664c67194f411ce4273b7d384
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-17 23:44:32 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,15 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Mengunduh File Pembaruan"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/autoscript-vpn/master/update/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/ryz-code/autoscript-vpn/master/websocket/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/ryz-code/autoscript-vpn/master/websocket/ws-stunnel
+
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/ryz-code/autoscript-vpn/master/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/ryz-code/autoscript-vpn/master/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 echo -e " [INFO] Perbarui Berhasil"
 sleep 2
 exit
