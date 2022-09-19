68db0adfbc099773f801bea98d105ead8895a46b
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-19 20:36:47 +0700

    Add files via upload

--- update-file/menu.sh
+++ update-file/menu.sh
@@ -75,6 +75,19 @@ fi
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
 
+#Download/Upload today
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
+ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
+tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+
 # // SSH Websocket Proxy
 ssh_ws=$( systemctl status ws-stunnel | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
 if [[ $ssh_ws == "running" ]]; then
@@ -175,6 +188,11 @@ echo -e "${COLOR1}┃$NC Memory Usage   : $uram / $tram"
 echo -e "${COLOR1}┃$NC ISP & City     : $ISP & $CITY"
 echo -e "${COLOR1}┃$NC Current Domain : $(cat /etc/xray/domain)"
 echo -e "${COLOR1}┃$NC IP-VPS         : ${COLOR1}$IPVPS${NC}"
+echo -e "\e[0;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[m"
+echo -e "\e[0;31mTRAFFIC         TODAY        YESTERDAY      MONTH\e[m"
+echo -e "\e[0;31mDownload\e[m        $dtoday G       $dyest G         $dmon G"
+echo -e "\e[0;31mUpload\e[m          $utoday G        $uyest G        $umon G"
+echo -e "\e[0;31mTotal\e[m           $ttoday G        $tyest G        $tmon G"
 echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
 echo -e "${COLOR1}┃$NC ${COLBG1}                 • VPS PANEL MENU •                    ${COLOR1}┃${NC}"
 echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫${NC}"
@@ -194,7 +212,7 @@ fi
 myver="$(cat /opt/.ver)"
 
 if [[ $serverV > $myver ]]; then
-echo -e "${COLOR1}┃$NC${COLOR1}[07]${NC} • MENU THEMES        [${COLOR1}14${NC}] • UPDATE TO V$1.2          ${COLOR1}┃${NC}"
+echo -e "${COLOR1}┃$NC${COLOR1}[07]${NC} • MENU THEMES        [${COLOR1}14${NC}] • UPDATE TO V$serverV          ${COLOR1}┃${NC}"
 echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫${NC}"
 up2u="updatews"
 else
@@ -229,12 +247,11 @@ case $opt in
 07 | 7) clear ; menu-theme ;;
 08 | 8) clear ; menu-backup ;;
 09 | 9) clear ; add-host ;;
-10) clear ; crt ;;
+10) clear ; crtxray ;;
 11) clear ; menu-set ;;
 12) clear ; info ;;
 13) clear ; $ressee ;;
-14) clear ; $bottt ;;
-100) clear ; $up2u ;;
+14) clear ; $up2u ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu ;;
 esac
