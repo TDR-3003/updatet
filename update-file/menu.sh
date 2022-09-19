5d8403b1a033278a1e49b91f1cf870f7bf5541a7
ryzXD <111454488+ryz-code@users.noreply.github.com>
2022-09-19 19:32:54 +0700

    Add files via upload

--- update-file/menu.sh
+++ update-file/menu.sh
@@ -33,6 +33,34 @@ BURIQ () {
     rm -f /root/tmp
 }
 
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/ryz-code/izin-vps/master/ipvps | grep $MYIP | awk '{print $2}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/ryz-code/izin-vps/master/ipvps | grep $MYIP | awk '{print $5}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/ryz-code/izin-vps/master/ipvps | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+
 x="ok"
 
 
@@ -70,6 +98,94 @@ if [[ $xray == "running" ]]; then
 else
     status_xray="${RED}OFF${NC}"
 fi
+
+function add-host(){
+clear
+echo -e "${COLOR1}┌─────────────────────────────────────────────────┐${NC}"
+echo -e "${COLOR1}│${NC} ${COLBG1}               • ADD VPS HOST •                ${NC} ${COLOR1}│$NC"
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo -e "${COLOR1}┌─────────────────────────────────────────────────┐${NC}"
+read -rp "  New Host Name : " -e host
+echo ""
+if [ -z $host ]; then
+echo -e "  [INFO] Type Your Domain/sub domain"
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo ""
+read -n 1 -s -r -p "  Press any key to back on menu"
+menu
+else
+echo "IP=$host" > /var/lib/ryzvpn-premium/ipvps.conf
+echo ""
+echo "  [INFO] Dont forget to renew cert"
+echo ""
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo ""
+read -n 1 -s -r -p "  Press any key to Renew Cret"
+crtxray
+fi
+}
+function updatews(){
+clear
+
+echo -e "${COLOR1}┌─────────────────────────────────────────────────┐${NC}"
+echo -e "${COLOR1}│${NC} ${COLBG1}            • UPDATE SCRIPT VPS •              ${NC} ${COLOR1}│$NC"
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo -e "${COLOR1}┌─────────────────────────────────────────────────┐${NC}"
+echo -e "${COLOR1}│${NC}  ${COLOR1}[INFO]${NC} Check for Script updates"
+sleep 2
+wget -q -O /root/install-up.sh "https://raw.githubusercontent.com/ryz-code/update/master/install-up.sh" && chmod +x /root/install-up.sh
+sleep 2
+./install-up.sh
+sleep 5
+rm /root/install-up.sh
+rm /opt/.ver
+version_up=$( curl -sS https://raw.githubusercontent.com/ryz-code/update/master/version-up)
+echo "$version-up" > /opt/.ver
+echo -e "${COLOR1}│${NC}  ${COLOR1}[INFO]${NC} Successfully Up To Date!"
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo -e "${COLOR1}┌────────────────────── BY ───────────────────────┐${NC}"
+echo -e "${COLOR1}│${NC}               • RYZ STORE VPN •                 ${COLOR1}│"
+echo -e "${COLOR1}└─────────────────────────────────────────────────┘${NC}"
+echo ""
+read -n 1 -s -r -p "  Press any key to back on menu"
+menu
+}
+clear
+echo -e "${COLOR1}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
+echo -e "${COLOR1}┃$NC ${COLBG1}                 • SYTEM INFORMATION •                 ${NC}┃"
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫${NC}"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+serverV=$( curl -sS https://raw.githubusercontent.com/ryz-code/update/master/version_up)
+if [ "$Isadmin" = "ON" ]; then
+uis="${GREEN}Premium User$NC"
+else
+uis="${RED}Free Version$NC"
+fi
+echo -e "${COLOR1}┃$NC User Roles     : $uis"
+if [ "$cekup" = "day" ]; then
+echo -e "${COLOR1}┃$NC System Uptime  : $uphours $upminutes $uptimecek"
+else
+echo -e "${COLOR1}┃$NC System Uptime  : $uphours $upminutes"
+fi
+echo -e "${COLOR1}┃$NC Memory Usage   : $uram / $tram"
+echo -e "${COLOR1}┃$NC ISP & City     : $ISP & $CITY"
+echo -e "${COLOR1}┃$NC Current Domain : $(cat /etc/xray/domain)"
+echo -e "${COLOR1}┃$NC IP-VPS         : ${COLOR1}$IPVPS${NC}"
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+echo -e "${COLOR1}┃$NC ${COLBG1}                 • VPS PANEL MENU •                    ${NC}┃"
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
+echo -e "${COLOR1}┃$NC [${COLOR1}01${NC}] • SSHWS          [${COLOR1}08${NC}] • BACKUP "   
+echo -e "${COLOR1}┃$NC [${COLOR1}02${NC}] • VMESS          [${COLOR1}09${NC}] • ADD HOST/DOMAIN   "  
+echo -e "${COLOR1}┃$NC [${COLOR1}03${NC}] • VLESS          [${COLOR1}10${NC}] • RENEW CERT"  
+echo -e "${COLOR1}┃$NC [${COLOR1}04${NC}] • TROJAN         [${COLOR1}11${NC}] • SETTINGS"  
+echo -e "${COLOR1}┃$NC [${COLOR1}05${NC}] • SS WS          [${COLOR1}12${NC}] • INFO "
+if [ "$Isadmin" = "ON" ]; then
+echo -e "                                                  ${COLOR1}│$NC"
+echo -e "${COLOR1}┃$NC [${COLOR1}06${NC}] • SET DNS        ${COLOR1}[13]${NC} • REG IP     "
 ressee="menu-ip"
 bottt="menu-bot"
 else
@@ -77,47 +193,35 @@ ressee="menu"
 bottt="menu"
 fi
 myver="$(cat /opt/.ver)"
+
 if [[ $serverV > $myver ]]; then
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
+echo -e "${COLOR1}┃$NC ${COLOR1}[100]${NC} • UPDATE TO V$serverV" 
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
 up2u="updatews"
 else
 up2u="menu"
 fi
+
 DATE=$(date +'%d %B %Y')
 datediff() {
     d1=$(date -d "$1" +%s)
     d2=$(date -d "$2" +%s)
+    echo -e "${COLOR1}Expiry In   : $(( (d1 - d2) / 86400 )) Days"
 }
 mai="datediff "$Exp" "$DATE""
-if [ $exp \> 1000 ]; then
+
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
+echo -e "${COLOR1}Version     :${NC} $(cat /opt/.ver) Latest Version"
+echo -e "${COLOR1}Client Name :${NC} $Name"
+if [ $exp \> 1000 ];
+then
+    echo -e "${COLOR1}License     :${NC} Lifetime"
 else
     datediff "$Exp" "$DATE"
 fi;
-
-# VPS MENU
-clear
-echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLOR1}System Uptime  :${NC} $uphours $upminutes $uptimecek"
-echo -e "${COLOR1}Memory Usage   :${NC} $uram / $tram"
-echo -e "${COLOR1}ISP & City     :${NC} $ISP & $CITY"
-echo -e "${COLOR1}Current Domain :${NC} $(cat /etc/xray/domain)"
-echo -e "${COLOR1}IP-VPS         :${NC} $IPVPS"
-echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLBG1}                    • VPS PANEL MENU •                    ${NC}"
-echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLOR1}[01]${NC} • SSHWS          ${COLOR1}[08]${NC} • BACKUP "   
-echo -e "${COLOR1}[02]${NC} • VMESS          ${COLOR1}[09]${NC} • ADD HOST/DOMAIN   "  
-echo -e "${COLOR1}[03]${NC} • VLESS          ${COLOR1}[10]${NC} • RENEW CERT"  
-echo -e "${COLOR1}[04]${NC} • TROJAN         ${COLOR1}[11]${NC} • SETTINGS"  
-echo -e "${COLOR1}[05]${NC} • SS WS          ${COLOR1}[12]${NC} • INFO "
-echo -e "${COLOR1}[06]${NC} • SET DNS        ${COLOR1}[13]${NC} • REG IP     "
-echo -e "${COLOR1}[07]${NC} • THEME          ${COLOR1}[14]${NC} • SET BOT  "
-echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLOR1}[100]${NC} • UPDATE TO V$serverV"
-echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLOR1}Version     :${NC} $(cat /opt/.ver) Latest Version"
-echo -e "${COLOR1}Client Name :${NC} $Name"
-echo -e "$COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━ BY ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${COLBG1}                    • RYZ STORE VPN •                     ${NC}"
+echo -e "${COLOR1}┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
+echo -e "${COLBG1}┃                    • RYZ STORE VPN •                     ┃${NC}"
 echo -e "${COLOR1}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e ""
 echo -ne " Select menu : "; read opt
